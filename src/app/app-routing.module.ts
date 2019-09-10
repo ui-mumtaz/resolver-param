ab83c808ac30fd603e693549711ec966beee9a6f
mumtaz <mumtazahmadui@gmail.com>
2019-09-09 23:43:35 -0700

    add strategy 2 for delay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { CustomPreloadingStrategy } from './custom-preloading-strategy';
 
 
 const routes: Routes = [
-  {path: 'company', loadChildren: './company/company.module#CompanyModule', data: { preload : true}},
-  {path: 'person', loadChildren: './person/person.module#PersonModule'},
+  {path: 'company', loadChildren: './company/company.module#CompanyModule', data: { preload : true, delay: true}},
+  {path: 'person', loadChildren: './person/person.module#PersonModule', data: { preload : true, delay: false}},
 
   {path : 'contactus', component:ContactusComponent},
   {path : '', redirectTo:'', pathMatch:'full'},
--- src/app/custom-preloading-strategy.ts
+++ src/app/custom-preloading-strategy.ts
@@ -1,16 +1,26 @@
 import { Injectable } from "@angular/core";
 import { PreloadingStrategy, Route } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, of, timer } from 'rxjs';
+import {mergeMap} from 'rxjs/operators';
 
 
 @Injectable()
 export class CustomPreloadingStrategy implements PreloadingStrategy {
     preload(route: Route, load:() => Observable<any>): Observable<any> {
         if(route.data && route.data['preload']) {
-            console.log('preload path is : ', route.path);
+            console.log('preload path : ' + route.path + ', delay:' + route.data['delay']);
+            if(route.data['delay']) {
+                return timer(5000).pipe(mergeMap(()=> load()));
+            }
             return load();
         } else {
             return of(null)
         }
+        // if(route.data && route.data['preload']) {
+        //     console.log('preload path is : ', route.path);
+        //     return load();
+        // } else {
+        //     return of(null)
+        // }
     }
 }
